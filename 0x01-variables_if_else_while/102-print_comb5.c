ebf7d2698a9b7555a7f4ac9ca4b30c7409bb8892
kofi-lartey <kofilartey12@gmail.com>
2023-06-20 08:30:56 -0700

    0-putchar.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,30 @@
 
 int main(void)
 {
-	int i = 00;
+	int i = 0;
 
 	while (i <= 98)
 	{
-		int j = 01;
+		int j = 0;
 
-		while (j <= 99)
+		while (j <= 9)
 		{
-			putchar (i / 10 + '0');
-			putchar (i % 10 + '0');
-			putchar (' ');
-			putchar (j / 10 + '0');
-			putchar (j % 10 + '0');
+			int y = 0;
 
-			if (i != 98 || j != 99)
-			{
-				putchar (',');
+			while (y <= 9)
+			{	putchar (i / 10 + '0');
+				putchar (i % 10 + '0');
 				putchar (' ');
+				putchar (j / 10 + '0');
+				putchar (y + '0');
+
+				if (i != 98 || j != 9 || y != 9)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+
+				y++;
 			}
 
 			j++;
@@ -35,6 +41,5 @@ int main(void)
 	}
 
 	putchar ('\n');
-
 	return (0);
 }
