ceacfb6c93ce74fec2bb9bc38bcc51a7a37d6332
kofi-lartey <kofilartey12@gmail.com>
2023-06-23 05:34:56 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most number excluding 2&4
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
